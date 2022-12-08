1357431a4c47a3b3008e0a9293082013ac79c216
unknown <zulifahmi1999@gmail.com>
2022-12-07 18:30:43 -0800

    kuliah hari ini 08/12/22

---
+++ pertemuan7/login/admin.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>admin page</title>
+</head>
+<body>
+    <h1>Selamat datang, admin!</h1>
+
+    <a href="login.php">logout</a>
+</body>
+</html>
